37ae804a7aaeacc5336db5aee2cba06828b51cf9
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-04 15:14:36 +0530

    Web API

--- WebAPIMovies/Models/Movie.cs
+++ WebAPIMovies/Models/Movie.cs
@@ -8,6 +8,7 @@ namespace WebAPIMovies.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Director { get; set; }
         public DateTime ReleaseDate { get; set; }
---
+++ WebAPIMovies/SQLQuery_Movie.sql
@@ -0,0 +1,10 @@
+create database Assignment17Db
+use Assignment17Db
+
+create table Movie
+(Id int primary key,
+Title nvarchar(50),
+Director nvarchar(50),
+ReleaseDate DateTime,
+Genre nvarchar(50),
+Rating float)
