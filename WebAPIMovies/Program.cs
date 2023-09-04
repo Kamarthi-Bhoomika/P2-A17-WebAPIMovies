3b98019e3e8d217bd345d35503976d758e9a0c3f
Kamarthi-Bhoomika <kbhoomika05@gmail.com>
2023-09-04 14:55:53 +0530

    Web API

---
+++ WebAPIMovies/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIMovies.Data;
+using WebAPIMovies.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDbContext") ?? throw new InvalidOperationException("Connection string 'MovieDbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddDbContext<Assignment17DbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDbContext")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
